24b990d02063980316dc2297de527c3b37970ba7
jovanigomez <jovani.gomez@gmail.com>
2022-05-24 00:35:49 -0700

    functions

---
+++ script.js
@@ -0,0 +1,69 @@
+const quizData = [ 
+    {
+        question: "ay ay ay ",
+        a:"jdjjd",
+        b:"ddrre",
+        c:"crccrc",
+        d:"dedrr",
+        correct: "c",
+    },
+
+    {
+        question: "ay ay ay ",
+        a:"jdjjd",
+        b:"ddrre",
+        c:"crccrc",
+        d:"dedrr",
+        correct: "c",
+    },
+
+    {
+        question: "ay ay ay ",
+        a:"jdjjd",
+        b:"ddrre",
+        c:"crccrc",
+        d:"dedrr",
+        correct: "c",
+    },
+
+    {
+        question: "ay ay ay ",
+        a:"jdjjd",
+        b:"ddrre",
+        c:"crccrc",
+        d:"dedrr",
+        correct: "c",
+    },
+
+    {
+        question: "ay ay ay ",
+        a:"jdjjd",
+        b:"ddrre",
+        c:"crccrc",
+        d:"dedrr",
+        correct: "c",
+    },
+
+    {
+        question: "ay ay ay ",
+        a:"jdjjd",
+        b:"ddrre",
+        c:"crccrc",
+        d:"dedrr",
+        correct: "c",
+    },
+
+
+
+
+
+
+];
+const  quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.insert')
+constquestionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
